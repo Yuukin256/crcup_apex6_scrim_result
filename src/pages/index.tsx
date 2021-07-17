84dd9481260dd49ed766209d96ea48ed6b43246b
Yuukin256 <Yuukin256@gmail.com>
2021-07-17 14:03:07 +0900

    注意書き追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,15 @@ const MainPage: React.FC = () => (
     <Seo title="Home" />
     <p>
       2021年7月23日に行われる 第6回 Crazy Raccoon Cup Apex Legends
-      の事前練習カスタム (スクリム) の試合結果一覧です。
+      の事前練習カスタム (スクリム)
+      の試合結果一覧です。正確性を保つ努力はしておりますが、集計に誤りがある可能性があります。
     </p>
-    <p className="note">
-      「CRキル数」はCR選手 (Ras, Selly, Zeder, Mondo, Cpt)
+    <p
+      style={{
+        fontSize: 'small',
+      }}
+    >
+      ※「CRキル数」はCR選手 (Ras, Selly, Zeder, Mondo, Cpt)
       に確定キルを入れた数です。確定キル1つにつきキルポイントとは別に2ポイント加算されます。
     </p>
     {data.reverse().map((day) => {
